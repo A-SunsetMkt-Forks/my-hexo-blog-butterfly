bafe2c2694e68086a72f2f6bb23a8355427dc307
Sunset Mikoto <26019675+lwd-temp@users.noreply.github.com>
2024-04-12 22:04:37 +0800

    loading.gif

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
 
 export default () => (
+	/**
 	gulp.src('src/images/*')
 		.pipe(imagemin())
 		.pipe(gulp.dest('dist/images'))
+	*/
+	/* Do nothing now */
+	console.log('gulpfile.js is unused now, this is just a placeholder')
 );
